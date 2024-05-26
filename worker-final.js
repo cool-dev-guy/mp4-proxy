9d052fc811a38654faa76b02de22cc585dc1a7e5
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-05-27 01:32:55 +0530

    Create worker-final.js

---
+++ worker-final.js
@@ -0,0 +1,31 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/proxy/')) {
+      const proxyUrl = atob(url.pathname.replace('/proxy/',''));
+      const proxyData = {
+        status:request.status,
+        statusText:request.statusText,
+        headers:request.headers
+      }
+      // a simple hack to bypass cloudflare not loading issue in firefox :)
+      const head  = await fetch(proxyUrl,{method:'HEAD'});
+      // it does a HEAD request and bypass the cloudflare issue.
+      const proxy = await fetch(proxyUrl,proxyData);
+      // simple hack to bypass cloudflare. :)
+      const headers = new Headers(proxy.headers);
+      headers.set('Access-Control-Allow-Origin', '*');
+      if (request.headers.get('range')===null||proxy.headers.get('Content-Range')===null){
+        headers.set("Accept-Ranges", "bytes")
+      }
+      // what it does is it mocks a Accept-Ranges to enable 206 partial-content
+      const responseData = {
+        status:proxy.status,
+        statusText:proxy.statusText,
+        headers:headers
+      }
+      return new Response(proxy.body,responseData);
+    }
+    else return new Response('Wrong path',{status:404});
+  },
+};
